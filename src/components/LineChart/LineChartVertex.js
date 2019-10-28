a9f2e0ba5fdc8ba0f5c21dc6b7e90002b419d089
Stuart Ki <stuartki@Tech-Daddy.local>
2019-10-28 10:41:17 -0400

    cleaned point functionality

--- src/components/LineChart/LineChartVertex.js
+++ src/components/LineChart/LineChartVertex.js
@@ -14,6 +14,9 @@ const propTypes = {
       y: React.PropTypes.number.isRequired,
     }),
   ).isRequired,
+  scHullVertices: PropTypes.arrayOf(
+
+  ).isRequired,
   isClicked: PropTypes.bool.isRequired,
   distanceToHull: PropTypes.number.isRequired,
   line: PropTypes.func.isRequired,
@@ -27,129 +30,15 @@ class Vertex extends React.Component {
       tielineClicked: false,
       tielineStay: false,
     };
-    this.timer = null;
     this.onClick = this.onClick.bind(this);
-    this.onMouseOver = this.onMouseOver.bind(this);
-    this.onMouseOut = this.onMouseOut.bind(this);
-    this.onLineClick = this.onLineClick.bind(this);
   }
 
   onClick() {
     this.props.pointClickHandler(this.props.auid);
   }
 
-  onMouseOver() {
-    this.setState({ tielineClicked: true });
-  }
-
-  onMouseOut() {
-    this.timer = setTimeout(() => {
-      this.setState({ tielineClicked: false });
-    }, 1000);
-  }
-
-  onLineClick() {
-    clearTimeout(this.timer);
-    this.setState({ tielineStay: !this.state.tielineStay });
-    if (!this.state.tielineStay) {
-      this.setState({ tielineClicked: false });
-    }
-  }
-
-  // fade(element) {
-  //   var op = 1;  // initial opacity
-  //   var timer = setInterval(function () {
-  //       if (op <= 0.1){
-  //           clearInterval(timer);
-  //           element.style.display = 'none';
-  //       }
-  //       element.style.opacity = op;
-  //       element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-  //       op -= op * 0.1;
-  //   }, 50);
-  // }
-
   render() {
-    // already inverted in scale
-    function hullDistance(endpoints, curX) {
-      const m = (endpoints[1].y - endpoints[0].y) / (endpoints[1].x - endpoints[0].x);
-      const b = endpoints[0].y - (m * endpoints[0].x);
-      return ((m * curX) + b);
-    }
-    let tieline = null;
     let point = null;
-    if (this.props.isClicked && this.props.distanceToHull > 0) {
-      let i;
-      let t;
-      let m;
-      const ver = this.props.vertices;
-
-      for (i = 0; i < ver.length; i++) {
-        if (this.props.xScale.invert(this.props.cx) < ver[i].x) {
-          m = ver.slice(i - 1, i + 1);
-          // to format tieline we add radius of circle
-          t = [
-            { x: m[0].x + this.props.xScale.invert(5), y: m[0].y },
-            { x: m[1].x, y: m[1].y },
-          ];
-          break;
-        }
-      }
-      const x = this.props.xScale.invert(this.props.cx);
-      const y = this.props.yScale.invert(this.props.cy);
-      const pathToHull = [
-        { x, y },
-        { x, y: hullDistance(t, x) },
-      ];
-      if (this.state.tielineClicked || this.state.tielineStay) {
-        let stroke = '#ff0000';
-        let className = 'line shadow';
-        if (this.state.tielineClicked) {
-          className = 'hline shadow';
-        }
-        if (this.state.tielineStay) {
-          className = 'tieline shadow';
-          stroke = 'green';
-        }
-        tieline =
-        (
-          <g>
-            <path
-              className={className}
-              stroke={stroke}
-              d={this.props.line(pathToHull)}
-              onClick={this.onLineClick}
-              strokeLinecap="round"
-              strokeDasharray="3, 10"
-            />
-            <path
-              className={className}
-              stroke={stroke}
-              d={this.props.line(t)}
-              strokeLinecap="round"
-            />
-            <circle
-              className="point"
-              r="6"
-              cx={this.props.xScale(m[1].x)}
-              cy={this.props.yScale(m[1].y)}
-              fill="none"
-              stroke="#ff0000"
-            />
-            <circle
-              className="point"
-              r="6"
-              cx={this.props.xScale(m[0].x)}
-              cy={this.props.yScale(m[0].y)}
-              fill="none"
-              stroke="#ff0000"
-            />
-
-          </g>
-        );
-      }
-    }
-
     point =
         (
           <g>
@@ -168,7 +57,6 @@ class Vertex extends React.Component {
         );
     return (
       <g>
-        {tieline}
         {point}
       </g>
     );
