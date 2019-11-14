38474f53b60a4e6a38084f92904a2543387e651e
Stuart Ki <stuartki@Tech-Daddy.local>
2019-11-14 13:21:03 -0500

    added Points object

---
+++ src/components/TernaryHull/render/TernaryPoints.js
@@ -0,0 +1,161 @@
+import * as THREE from 'three';
+// drawing points
+
+export default class TernaryPoints {
+  constructor(data, gridHeight) {
+    this.data = data;
+
+    this.gridHeight = gridHeight;
+    this.triSide = this.gridHeight * 2;
+    this.triMargin = 0;
+    this.triHeight = (Math.sqrt(3) / (2)) * this.triSide;
+    this.triCenter = this.triCoord(33, 33, 34);
+    this.hullGroup = new THREE.Group();
+  }
+
+  plotEntries(data) {
+    // clear points
+    this.group.remove(this.pointCloud);
+
+    const entries = this.filterByEnthalpy(data);
+
+    // create new geometry
+    const pointsGeometry = new THREE.BufferGeometry();
+
+    const positions = new Float32Array(entries.length * 3);
+    const colors = new Float32Array(entries.length * 3);
+    const sizes = new Float32Array(entries.length);
+
+    const auids = [];
+    // const sprite = THREE.ImageUtils.loadTexture('textures/disc.png');
+
+    for (let i = 0; i < entries.length; i++) {
+      const pX = entries[i].composition[0] * 100;
+      const pY = entries[i].composition[2] * 100;
+      const pZ = entries[i].composition[1] * 100;
+      const pCoord = this.triCoord(pX, pY, pZ);
+
+      const datapoint = new THREE.Vector3(
+        pCoord[0],
+        pCoord[1],
+        (entries[i].enthalpyFormationAtom * this.gridHeight),
+      );
+      let pointColor;
+      if (this.defaultColor) {
+        pointColor = new THREE.Color(pX / 100, pY / 100, pZ / 100);
+      } else {
+        pointColor = this.colorVertex(datapoint);
+      }
+      let size = 40;
+      if (entries[i].isClicked) {
+        pointColor = new THREE.Color('#CA6F96');
+        size = 80;
+      }
+      datapoint.toArray(positions, i * 3);
+      pointColor.toArray(colors, i * 3);
+      sizes[i] = size;
+      auids[i] = entries[i].auid;
+    }
+
+    pointsGeometry.addAttribute(
+      'position',
+      new THREE.BufferAttribute(positions, 3),
+    );
+    pointsGeometry.addAttribute(
+      'customColor',
+      new THREE.BufferAttribute(colors, 3),
+    );
+    pointsGeometry.addAttribute(
+      'size',
+      new THREE.BufferAttribute(sizes, 1),
+    );
+
+    const fragmentShader = [
+      'uniform vec3 color;',
+      'uniform sampler2D texture;',
+      'varying vec3 vColor;',
+      'void main() {',
+      ' gl_FragColor = vec4( color * vColor, 1.0 );',
+      ' gl_FragColor = gl_FragColor * texture2D( texture, gl_PointCoord );',
+      ' if ( gl_FragColor.a < ALPHATEST ) discard;',
+      '}',
+    ].join('\n');
+
+    const vertexShader = [
+      'attribute float size;',
+      'attribute vec3 customColor;',
+      'varying vec3 vColor;',
+      'void main() {',
+      '  vColor = customColor;',
+      '  vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );',
+      '  gl_PointSize = size * ( 300.0 / -mvPosition.z );',
+      '  gl_Position = projectionMatrix * mvPosition;',
+      '}',
+    ].join('\n');
+
+    const texture = new THREE.TextureLoader().load('textures/ball.png');
+    const pointsMaterial = new THREE.ShaderMaterial({
+      uniforms: {
+        color: { type: 'c', value: new THREE.Color(0xFFFFFF) },
+        texture: { type: 't', value: texture },
+      },
+      vertexShader,
+      fragmentShader,
+      alphaTest: 0.9,
+    });
+    this.pointCloud = new THREE.Points(pointsGeometry, pointsMaterial);
+    this.pointCloud.pointNames = auids;
+    this.intersectArray = [];
+    this.intersectArray.push(this.pointCloud);
+    this.group.add(this.pointCloud);
+  }
+
+  updatePlottedEntries(data) {
+    const entries = this.filterByEnthalpy(data);
+    const positions = new Float32Array(entries.length * 3);
+    const colors = new Float32Array(entries.length * 3);
+    const sizes = new Float32Array(entries.length);
+
+    const auids = [];
+    // const sprite = THREE.ImageUtils.loadTexture('textures/disc.png');
+
+    for (let i = 0; i < entries.length; i++) {
+      const pX = entries[i].composition[0] * 100;
+      const pY = entries[i].composition[2] * 100;
+      const pZ = entries[i].composition[1] * 100;
+      const pCoord = this.triCoord(pX, pY, pZ);
+
+      const datapoint = new THREE.Vector3(
+        pCoord[0],
+        pCoord[1],
+        (entries[i].enthalpyFormationAtom * this.gridHeight),
+      );
+      let pointColor;
+      if (this.defaultColor) {
+        pointColor = new THREE.Color(pX / 100, pY / 100, pZ / 100);
+      } else {
+        pointColor = this.colorVertex(datapoint);
+      }
+      let size = 40;
+      if (entries[i].isClicked) {
+        pointColor = new THREE.Color('#CA6F96');
+        size = 80;
+      }
+      datapoint.toArray(positions, i * 3);
+      pointColor.toArray(colors, i * 3);
+      sizes[i] = size;
+      auids[i] = entries[i].auid;
+    }
+
+    this.pointCloud.geometry.attributes.size.set(sizes);
+    this.pointCloud.geometry.attributes.size.needsUpdate = true;
+
+    this.pointCloud.geometry.attributes.customColor.set(colors);
+    this.pointCloud.geometry.attributes.customColor.needsUpdate = true;
+
+    this.pointCloud.geometry.attributes.position.set(positions);
+    this.pointCloud.geometry.attributes.position.needsUpdate = true;
+
+    this.pointCloud.pointNames = auids;
+  }
+}
