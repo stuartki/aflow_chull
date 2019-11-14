5d75208f93f2b8815de80271b00389d1d1343f70
Stuart Ki <stuartki@Tech-Daddy.local>
2019-11-14 13:15:50 -0500

    added triCoord scaling to TGrid and made a class definition

--- src/components/TernaryHull/render/TernaryGrid.js
+++ src/components/TernaryHull/render/TernaryGrid.js
@@ -1,130 +1,161 @@
 import * as THREE from 'three';
 
-export default function drawGrid(gridHeight, triSide, triMargin, triHeight) {
-    // ++++++++++++++++++ DRAWING TRIANGULAR GRID ++++++++++++++++++++++++++++++
-    //        this can be cleaner but for now it will do...
 
-  const material = new THREE.LineBasicMaterial({
-    color: 0x000000,
-  });
+export default class TernaryGrid {
+  constructor(gridHeight = 300, triMargin = 0) {
+    this.gridHeight = gridHeight;
+    this.triMargin = triMargin;
 
-    // lower geometry
-  const gridLowerGeometry = new THREE.Geometry();
-  gridLowerGeometry.vertices.push(
-      new THREE.Vector3(0, 0, -gridHeight),
-    );
-  gridLowerGeometry.vertices.push(
-      new THREE.Vector3(
-        (triSide / 2) + triMargin,
-        triHeight + triMargin,
-        -gridHeight,
-      ),
-    );
-  gridLowerGeometry.vertices.push(
-      new THREE.Vector3(
-        triSide + triMargin,
-        0,
-        -gridHeight,
-      ),
-    );
-  gridLowerGeometry.vertices.push(
-      new THREE.Vector3(0, 0, -gridHeight),
-    );
+    this.triSide = this.gridHeight * 2;
+    this.triHeight = (Math.sqrt(3) / (2)) * this.triSide;
+    this.triCenter = this.triCoord(33, 33, 34);
+    this.grid = new THREE.Group();
+  }
 
-    // mid geometry
-  const gridMidGeometry = new THREE.Geometry();
-  gridMidGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
-  gridMidGeometry.vertices.push(
-      new THREE.Vector3(
-        (triSide / 2) + triMargin,
-        triHeight + triMargin,
-        0,
-      ),
-    );
-  gridMidGeometry.vertices.push(
-      new THREE.Vector3(triSide + triMargin, 0, 0),
-    );
-  gridMidGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  triCoord(a, b, c) {
+    let sum = [0, 0];
+    const pos = [0, 0];
+    const corners = [
+      [this.triMargin, 0],
+      [this.triSide + this.triMargin, 0],
+      [(this.triSide / 2) + this.triMargin, this.triHeight + this.triMargin],
+    ];
+    sum = a + b + c;
+    if (sum !== 0) {
+      const x = a / sum;
+      const y = b / sum;
+      const z = c / sum;
+      pos[0] = (corners[0][0] * x) + (corners[1][0] * y) + (corners[2][0] * z);
+      pos[1] = (corners[0][1] * x) + (corners[1][1] * y) + (corners[2][1] * z);
+    }
+    return pos;
+  }
 
-    // upper geometry
-  const gridUpperGeometry = new THREE.Geometry();
-  gridUpperGeometry.vertices.push(new THREE.Vector3(0, 0, gridHeight));
-  gridUpperGeometry.vertices.push(
-      new THREE.Vector3(
-        (triSide / 2) + triMargin,
-        triHeight + triMargin,
-        gridHeight,
-      ),
-    );
-  gridUpperGeometry.vertices.push(
-      new THREE.Vector3(
-        triSide + triMargin,
-        0,
-        gridHeight,
-      ),
-    );
-  gridUpperGeometry.vertices.push(new THREE.Vector3(0, 0, gridHeight));
+  drawGrid() {
+      // ++++++++++++++++++ DRAWING TRIANGULAR GRID ++++++++++++++++++++++++++++++
+      //        this can be cleaner but for now it will do...
 
-    // SIDE GEOMETRY (lines?)
-    // side one
-  const gridSideOneGeometry = new THREE.Geometry();
-  gridSideOneGeometry.vertices.push(
-      new THREE.Vector3(0, 0, -gridHeight),
-    );
-  gridSideOneGeometry.vertices.push(
-      new THREE.Vector3(0, 0, gridHeight),
-    );
+    const material = new THREE.LineBasicMaterial({
+      color: 0x000000,
+    });
 
-    // side two
-  const gridSideTwoGeometry = new THREE.Geometry();
-  gridSideTwoGeometry.vertices.push(
-      new THREE.Vector3(
-        (triSide / 2) + triMargin,
-        triHeight + triMargin,
-        -gridHeight,
-      ),
-    );
-  gridSideTwoGeometry.vertices.push(
-      new THREE.Vector3(
-        (triSide / 2) + triMargin,
-        triHeight + triMargin,
-        gridHeight,
-      ),
-    );
+      // lower geometry
+    const gridLowerGeometry = new THREE.Geometry();
+    gridLowerGeometry.vertices.push(
+        new THREE.Vector3(0, 0, -this.gridHeight),
+      );
+    gridLowerGeometry.vertices.push(
+        new THREE.Vector3(
+          (this.triSide / 2) + this.triMargin,
+          this.triHeight + this.triMargin,
+          -this.gridHeight,
+        ),
+      );
+    gridLowerGeometry.vertices.push(
+        new THREE.Vector3(
+          this.triSide + this.triMargin,
+          0,
+          -this.gridHeight,
+        ),
+      );
+    gridLowerGeometry.vertices.push(
+        new THREE.Vector3(0, 0, -this.gridHeight),
+      );
 
-    // side three
-  const gridSideThreeGeometry = new THREE.Geometry();
-  gridSideThreeGeometry.vertices.push(
-      new THREE.Vector3(
-        triSide + triMargin,
-        0,
-        -gridHeight,
-      ),
-    );
-  gridSideThreeGeometry.vertices.push(
-      new THREE.Vector3(
-        triSide + triMargin,
-        0,
-        gridHeight,
-      ),
-    );
+      // mid geometry
+    const gridMidGeometry = new THREE.Geometry();
+    gridMidGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
+    gridMidGeometry.vertices.push(
+        new THREE.Vector3(
+          (this.triSide / 2) + this.triMargin,
+          this.triHeight + this.triMargin,
+          0,
+        ),
+      );
+    gridMidGeometry.vertices.push(
+        new THREE.Vector3(this.triSide + this.triMargin, 0, 0),
+      );
+    gridMidGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
 
-    // constructing lines
-  const gridLineLower = new THREE.Line(gridLowerGeometry, material);
-  const gridLineMid = new THREE.Line(gridMidGeometry, material);
-  const gridLineUpper = new THREE.Line(gridUpperGeometry, material);
+      // upper geometry
+    const gridUpperGeometry = new THREE.Geometry();
+    gridUpperGeometry.vertices.push(new THREE.Vector3(0, 0, this.gridHeight));
+    gridUpperGeometry.vertices.push(
+        new THREE.Vector3(
+          (this.triSide / 2) + this.triMargin,
+          this.triHeight + this.triMargin,
+          this.gridHeight,
+        ),
+      );
+    gridUpperGeometry.vertices.push(
+        new THREE.Vector3(
+          this.triSide + this.triMargin,
+          0,
+          this.gridHeight,
+        ),
+      );
+    gridUpperGeometry.vertices.push(new THREE.Vector3(0, 0, this.gridHeight));
 
-  const gridLineSideOne = new THREE.Line(gridSideOneGeometry, material);
-  const gridLineSideTwo = new THREE.Line(gridSideTwoGeometry, material);
-  const gridLineSideThree = new THREE.Line(gridSideThreeGeometry, material);
+      // SIDE GEOMETRY (lines?)
+      // side one
+    const gridSideOneGeometry = new THREE.Geometry();
+    gridSideOneGeometry.vertices.push(
+        new THREE.Vector3(0, 0, -this.gridHeight),
+      );
+    gridSideOneGeometry.vertices.push(
+        new THREE.Vector3(0, 0, this.gridHeight),
+      );
 
-  const group = new THREE.Group();
-  group.add(gridLineLower);
-  group.add(gridLineMid);
-  group.add(gridLineUpper);
-  group.add(gridLineSideOne);
-  group.add(gridLineSideTwo);
-  group.add(gridLineSideThree);
+      // side two
+    const gridSideTwoGeometry = new THREE.Geometry();
+    gridSideTwoGeometry.vertices.push(
+        new THREE.Vector3(
+          (this.triSide / 2) + this.triMargin,
+          this.triHeight + this.triMargin,
+          -this.gridHeight,
+        ),
+      );
+    gridSideTwoGeometry.vertices.push(
+        new THREE.Vector3(
+          (this.triSide / 2) + this.triMargin,
+          this.triHeight + this.triMargin,
+          this.gridHeight,
+        ),
+      );
 
-  return group;
+      // side three
+    const gridSideThreeGeometry = new THREE.Geometry();
+    gridSideThreeGeometry.vertices.push(
+        new THREE.Vector3(
+          this.triSide + this.triMargin,
+          0,
+          -this.gridHeight,
+        ),
+      );
+    gridSideThreeGeometry.vertices.push(
+        new THREE.Vector3(
+          this.triSide + this.triMargin,
+          0,
+          this.gridHeight,
+        ),
+      );
+
+      // constructing lines
+    const gridLineLower = new THREE.Line(gridLowerGeometry, material);
+    const gridLineMid = new THREE.Line(gridMidGeometry, material);
+    const gridLineUpper = new THREE.Line(gridUpperGeometry, material);
+
+    const gridLineSideOne = new THREE.Line(gridSideOneGeometry, material);
+    const gridLineSideTwo = new THREE.Line(gridSideTwoGeometry, material);
+    const gridLineSideThree = new THREE.Line(gridSideThreeGeometry, material);
+
+    this.grid.add(gridLineLower);
+    this.grid.add(gridLineMid);
+    this.grid.add(gridLineUpper);
+    this.grid.add(gridLineSideOne);
+    this.grid.add(gridLineSideTwo);
+    this.grid.add(gridLineSideThree);
+
+    return this.grid;
+  }
 }
