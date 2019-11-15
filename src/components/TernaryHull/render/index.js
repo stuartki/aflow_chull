47352d4ea904153f6145a4b6a494d3bc4f0d4f34
Stuart Ki <stuartki@Tech-Daddy.local>
2019-11-15 12:44:23 -0500

    adding Points

--- src/components/TernaryHull/render/index.js
+++ src/components/TernaryHull/render/index.js
@@ -128,97 +128,7 @@ class TernaryHullRender {
   }
 
   plotEntries(data) {
-    // clear points
-    this.group.remove(this.pointCloud);
-
-    const entries = this.filterByEnthalpy(data);
-
-    // create new geometry
-    const pointsGeometry = new THREE.BufferGeometry();
-
-    const positions = new Float32Array(entries.length * 3);
-    const colors = new Float32Array(entries.length * 3);
-    const sizes = new Float32Array(entries.length);
-
-    const auids = [];
-    // const sprite = THREE.ImageUtils.loadTexture('textures/disc.png');
-
-    for (let i = 0; i < entries.length; i++) {
-      const pX = entries[i].composition[0] * 100;
-      const pY = entries[i].composition[2] * 100;
-      const pZ = entries[i].composition[1] * 100;
-      const pCoord = this.TGrid.triCoord(pX, pY, pZ);
-
-      const datapoint = new THREE.Vector3(
-        pCoord[0],
-        pCoord[1],
-        (entries[i].enthalpyFormationAtom * this.gridHeight),
-      );
-      let pointColor;
-      if (this.defaultColor) {
-        pointColor = new THREE.Color(pX / 100, pY / 100, pZ / 100);
-      } else {
-        pointColor = this.colorVertex(datapoint);
-      }
-      let size = 40;
-      if (entries[i].isClicked) {
-        pointColor = new THREE.Color('#CA6F96');
-        size = 80;
-      }
-      datapoint.toArray(positions, i * 3);
-      pointColor.toArray(colors, i * 3);
-      sizes[i] = size;
-      auids[i] = entries[i].auid;
-    }
-
-    pointsGeometry.addAttribute(
-      'position',
-      new THREE.BufferAttribute(positions, 3),
-    );
-    pointsGeometry.addAttribute(
-      'customColor',
-      new THREE.BufferAttribute(colors, 3),
-    );
-    pointsGeometry.addAttribute(
-      'size',
-      new THREE.BufferAttribute(sizes, 1),
-    );
-
-    const fragmentShader = [
-      'uniform vec3 color;',
-      'uniform sampler2D texture;',
-      'varying vec3 vColor;',
-      'void main() {',
-      ' gl_FragColor = vec4( color * vColor, 1.0 );',
-      ' gl_FragColor = gl_FragColor * texture2D( texture, gl_PointCoord );',
-      ' if ( gl_FragColor.a < ALPHATEST ) discard;',
-      '}',
-    ].join('\n');
-
-    const vertexShader = [
-      'attribute float size;',
-      'attribute vec3 customColor;',
-      'varying vec3 vColor;',
-      'void main() {',
-      '  vColor = customColor;',
-      '  vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );',
-      '  gl_PointSize = size * ( 300.0 / -mvPosition.z );',
-      '  gl_Position = projectionMatrix * mvPosition;',
-      '}',
-    ].join('\n');
-
-    const texture = new THREE.TextureLoader().load('textures/ball.png');
-    const pointsMaterial = new THREE.ShaderMaterial({
-      uniforms: {
-        color: { type: 'c', value: new THREE.Color(0xFFFFFF) },
-        texture: { type: 't', value: texture },
-      },
-      vertexShader,
-      fragmentShader,
-      alphaTest: 0.9,
-    });
-    this.pointCloud = new THREE.Points(pointsGeometry, pointsMaterial);
-    this.pointCloud.pointNames = auids;
+    this.pointCloud = this.TPoints.plotEntries(data);
     this.intersectArray = [];
     this.intersectArray.push(this.pointCloud);
     this.group.add(this.pointCloud);
@@ -256,7 +166,7 @@ class TernaryHullRender {
         size = 80;
       } else {
         // eslint-disable-next-line max-len
-        pointColor = new THREE.Color(...this.pointCloud.geometry.attributes.customColor.array.slice(i * 3, (i * 3) + 3));
+        pointColor = new THREE.Color(entries[i].composition[0], entries[i].composition[2], entries[i].composition[1]);
       }
       // datapoint.toArray(positions, i * 3);
       pointColor.toArray(colors, i * 3);
