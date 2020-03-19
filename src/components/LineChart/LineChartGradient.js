e1727a16288029a8e0943525885d7ee2d76f115b
Stuart Ki <stuartki@Tech-Daddy.local>
2020-03-19 18:27:18 -0400

    changed opacity to 0.8 and made offset 90%

--- src/components/LineChart/LineChartGradient.js
+++ src/components/LineChart/LineChartGradient.js
@@ -12,7 +12,7 @@ const Gradient = props => (
   <defs>
     <linearGradient is id={props.id} x1="0%" y1="100%" x2="0%" y2="0%" spreadMethod="pad">
       <stop is offset="5%" stop-color={props.color1} stop-opacity={0.4} />
-      <stop is offset="80%" stop-color={props.color2} stop-opacity={1} />
+      <stop is offset="90%" stop-color={props.color2} stop-opacity={0.8} />
     </linearGradient>
   </defs>
 );
