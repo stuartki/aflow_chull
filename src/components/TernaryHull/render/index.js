a6fe5e1ff4181591cc422aba7be812cb0893a8e0
Stuart Ki <stuartki@tech-daddy.home>
2020-06-01 16:06:07 -0400

    added comments to index.js render file

--- src/components/TernaryHull/render/index.js
+++ src/components/TernaryHull/render/index.js
@@ -168,7 +168,7 @@ class TernaryHullRender {
     }
   }
 
-  // create different camera views
+  // in-class method to set different camera views
   setCamera(type) {
     if (type === 'init') {
       this.camera.up = new THREE.Vector3(0, 0, 1);
@@ -181,72 +181,96 @@ class TernaryHullRender {
     }
   }
 
-  // plotEntries removes previous data as this is the workhorse to plot new initial data
+  // plotEntries removes previous data in order to plot new initial data
   plotEntries(showThree = false) {
+    // removes existing pointCloud
     this.group.remove(this.pointCloud);
+
+    // plots using TPoints object
     this.pointCloud = this.TPoints.plotEntries(showThree);
+
+    // adding intersect boundaries
     this.intersectArray = [];
     this.intersectArray.push(this.pointCloud);
     this.group.add(this.pointCloud);
   }
 
-  // slightly cleaned so that it only updates colors and sizes
+
   // solely updates to selectedPointCloud, preserves this.pointCloud original data
+  // updates colors and sizes
   updatePlottedEntries(data, defaultBehavior) {
+    // collects new data attributes
     const entries = this.TPoints.filterMinMaxGrid(data);
+    // removes any hull lines/decomposition lines
     this.lineGroup.remove(...this.lineGroup.children);
+
+    // new colors and sizes arrays (positions are unchanged)
     const colors = new Float32Array(entries.length * 3);
     const sizes = new Float32Array(entries.length);
 
     const selectedData = [];
+
+    // indicates whether a point is clicked
     let click = false;
 
     const auids = [];
     // const sprite = THREE.ImageUtils.loadTexture('textures/disc.png');
 
-    // loops through and finds all clicked entriese
+    // loops through and finds all clicked entries
     for (let i = 0; i < entries.length; i++) {
       let size = 40;
       let pointColor;
       if (entries[i].isClicked) {
+        // pushes to selected data
         selectedData.push(i);
         click = true;
+        // sets clicked format
         pointColor = new THREE.Color('#CA6F96');
         size = 80;
       } else {
+        // default color
         // eslint-disable-next-line max-len
         pointColor = new THREE.Color(entries[i].composition[0], entries[i].composition[2], entries[i].composition[1]);
       }
-      // datapoint.toArray(positions, i * 3);
+
+      // adds to color/size arrays
       pointColor.toArray(colors, i * 3);
       sizes[i] = size;
       auids[i] = entries[i].auid;
     }
 
+    // if there is a clicked point (so that we don't run on non-clicked instances)
     if (click) {
+      // need new sPositions array to record decomposition points
       const sPositions = new Float32Array(entries.length * 3);
       const sColors = new Float32Array(entries.length * 3);
       const sSizes = new Float32Array(entries.length);
       const sAuids = [];
+
+      // current iteration is to have a moving index
       let count = 0;
       for (let s = 0; s < selectedData.length; s++) {
         const pt = entries[selectedData[s]];
 
+        // standard formatting
         const size = 80;
         const pointColor = new THREE.Color('#CA6F96');
 
-
+        // slice current point position data
         const spos = this.pointCloud.geometry.attributes.position.array.slice(
           selectedData[s] * 3, (selectedData[s] * 3) + 3,
         );
+        // add these positions to selectedPointCloud positions
+        // adding at "count", which is the number of points added so far
         for (let j = 0; j < 3; j++) {
           sPositions[count * 3 + j] = spos[j];
         }
+        // add formatting
         pointColor.toArray(sColors, count * 3);
         sSizes[count] = size;
         sAuids.push(entries[selectedData[s]].auid);
 
-        // handles null decomposition points by finding minimum same compound
+        // handles null decomposition points by finding same compound with minimum energy
         let decompPoints;
         if (pt.decompositionAuids === null || pt.decompositionAuids === undefined) {
           decompPoints = entries.filter(d => pt.composition[0] === d.composition[0] && pt.composition[1] === d.composition[1] && pt.composition[2] === d.composition[2]);
@@ -255,10 +279,15 @@ class TernaryHullRender {
           decompPoints = entries.filter(d => pt.decompositionAuids.includes(d.auid));
         }
         
-        // aadds decomposition points to selectedPointCloud
+        // adds decomposition points to selectedPointCloud
         for (let dind = 0; dind < decompPoints.length; dind++) {
+          // handles variable decomposiiton points by moving count index up 1 each decomp point
           count += 1;
+
+          // retrives point from decomposition point array
           const d = decompPoints[dind];
+
+          // retrieves position
           const pX = d.composition[0] * 100;
           const pY = d.composition[2] * 100;
           const pZ = d.composition[1] * 100;
@@ -269,24 +298,31 @@ class TernaryHullRender {
             pCoord[1],
             (d.enthalpyFormationAtom * this.TGrid.gridHeight),
           );
+
+          // retrieves color
           let dpointColor;
           if (this.defaultColor) {
             dpointColor = new THREE.Color(pX / 100, pY / 100, pZ / 100);
           } else {
             dpointColor = this.colorVertex(datapoint);
           }
+
+          // adds to position, color, and size arrays
           datapoint.toArray(sPositions, count * 3);
           dpointColor.toArray(sColors, count * 3);
           sSizes[count] = size;
           sAuids.push(d.auid);
         }
         // count handles movement of indices in buffer attributes
+        // adds one for next point
         count += 1;
         // break indicates end of set of decomposition points
+        // current solution for variable length decomposition points
         sAuids.push('break');
         count += 1;
       }
 
+      // local index allows selective drawing range for selectedPointCloud
       this.TPoints.selectedPointCloud.geometry.setDrawRange(0, count);
 
       this.TPoints.selectedPointCloud.geometry.attributes.size.set(sSizes);
@@ -301,17 +337,22 @@ class TernaryHullRender {
       this.TPoints.selectedPointCloud.pointNames = sAuids;
 
       if (defaultBehavior) {
-        // rescales points threshold
+        // rescales points threshold to larger points since defaultBehavior allows only one point shown
         this.raycaster.params.Points.threshold = 12;
+
+        // removes current pointCloud to solely show selectedPointCloud
         this.group.remove(this.pointCloud);
+        // cleans intersectArray
         this.intersectArray = [];
       } else {
+        // if not defaultBehavior, add pointCloud to scene
         this.raycaster.params.Points.threshold = 7;
         this.intersectArray = [this.pointCloud];
       }
+      // add selected points to scene
       this.group.add(this.TPoints.selectedPointCloud);
       this.intersectArray.push(this.TPoints.selectedPointCloud);
-    } else {
+    } else { // condition if there is no selected point in data
       this.raycaster.params.Points.threshold = 7;
       this.group.remove(this.TPoints.selectedPointCloud);
       this.group.add(this.pointCloud);
@@ -325,6 +366,7 @@ class TernaryHullRender {
     this.pointCloud.geometry.attributes.customColor.set(colors);
     this.pointCloud.geometry.attributes.customColor.needsUpdate = true;
 
+    // NO LONGER UPDATES POSITIONS
     // this.pointCloud.geometry.attributes.position.set(positions);
     // this.pointCloud.geometry.attributes.position.needsUpdate = true;
 
@@ -369,6 +411,12 @@ class TernaryHullRender {
       return !(hasNeg && hasPos);
     }
 
+    // make THREE vector from array of point positions
+    function makePoint(pt) {
+      return new THREE.Vector3(pt[0], pt[1], pt[2]);
+    }
+
+    // making cylinder line (can control width of line)
     function makeLine(v1, v2) {
       /* edge from X to Y */
       const direction = new THREE.Vector3().subVectors(v2, v1);
@@ -400,7 +448,11 @@ class TernaryHullRender {
       // return new THREE.Line(geometry, material);
     }
 
+    // finding projected point on hull
+    // v1 is a point on the plane below the hull
+    // relies on normals
     function hullPoint(normal, v1, curPoint) {
+      // clone to make sure we do not manipulate current pt data
       const pt = curPoint.clone();
       const ptOnPlane = v1.clone();
       pt.add(ptOnPlane.multiplyScalar(-1));
@@ -412,10 +464,9 @@ class TernaryHullRender {
     const faces = hullMesh.geometry.faces;
     const vertices = hullMesh.geometry.vertices;
 
-    let vertex1;
-    let vertex2;
-    let vertex3;
     let count = 1;
+
+    // retrieve the selected and decomposition points from selectedPointCloud
     const selectedData = [];
     const index = this.TPoints.selectedPointCloud.pointNames.indexOf(auid);
     while (this.TPoints.selectedPointCloud.pointNames[index + count] !== 'break') {
@@ -424,23 +475,27 @@ class TernaryHullRender {
       count += 1;
     }
 
-    function makePoint(pt) {
-      return new THREE.Vector3(pt[0], pt[1], pt[2]);
-    }
+
+    let vertex1;
+    let vertex2;
+    let vertex3;
     const thisPoint = makePoint(point);
+    // loop through the faces to find the facet of hull that the point is over
     for (let i = 0; i < faces.length; i++) {
       vertex1 = vertices[faces[i].a];
       vertex2 = vertices[faces[i].b];
       vertex3 = vertices[faces[i].c];
       if (inTriangle(thisPoint, [vertex1, vertex2, vertex3])) {
-        const p = hullPoint(faces[i].normal, vertex1, makePoint(point));
-        this.lineGroup.add(makeLine(makePoint(point), p));
+        const p = hullPoint(faces[i].normal, vertex1, thisPoint);
+        this.lineGroup.add(makeLine(thisPoint, p));
         selectedData.forEach(d => this.lineGroup.add(makeLine(makePoint(d), p)));
       }
     }
   }
 
+  // indicates what type of point it is (for point handling)
   pointIndicator(auid) {
+    // quick check if point is a binary point
     function isBinPoint(entries, i) {
       const pX = !entries[i].composition[0];
       const pY = !entries[i].composition[2];
@@ -458,15 +513,24 @@ class TernaryHullRender {
 
     const isVertex = this.hull.entries[index].distanceToHull === 0;
     const isClicked = this.hull.entries[index].isClicked;
+
+    // different conditions
+    // 1 will show decomposition points
+
+    // CLICKED, NONBINARY, NONVERTEX point
     if (isClicked && !isBinPoint(this.hull.entries, index) && !isVertex) {
       return 1;
+    // CLICKED, BINARY, NONVERTEX point
     } else if (isClicked && isBinPoint(this.hull.entries, index) && !isVertex) {
       return 1;
+    // CLICKED, VERTEX point
     } else if (isClicked && isVertex) {
       return 3;
+    // CLICKED point
     } else if (isClicked) {
       return 4;
     }
+    // null
     return -1;
   }
 
@@ -493,37 +557,68 @@ class TernaryHullRender {
     const intersections = this.raycaster.intersectObjects(this.intersectArray);
     const intersection = (intersections.length) > 0 ? intersections[0] : null;
 
+    // debug ray
     // this.ray.geometry.vertices[0] = new THREE.Vector3(this.TGrid.triCenter[0], - (5 * this.TGrid.triCenter[1]), 0);
     // this.ray.geometry.vertices[1] = this.raycaster.ray.direction.multiplyScalar(1000).add(this.raycaster.ray.origin);
     // this.ray.geometry.verticesNeedUpdate = true;
 
     if (intersection !== null) {
+      // handling movement of pointer
       if (this.showPointer) {
         this.sphere.position.copy(intersection.point);
       }
+
+      // retrieve auid from pointNames in intersection object
       const auid = intersection.object.pointNames[intersection.index];
+
+      // retrieve indicator number
       const indicator = this.pointIndicator(auid);
+
+      // retrieve the index from pointNames array
       const index = this.pointCloud.pointNames.indexOf(auid);
+
+      // retrieve the position from position array
       const pt = this.pointCloud.geometry.attributes.position.array.slice(index * 3, index * 3 + 3);
+
+      // the following only happen for non default behavior
+      // CLICKED, NONBINARY/BINARY, NONVERTEX point
       if (indicator === 1 && !this.defaultBehavior) {
+        // add to lineGroup the distance to hull/decomposition points
         this.distanceToHull(pt, index, this.THull.hullMesh);
+        // add to scene
         this.group.add(this.lineGroup);
+      // CLICKED, VERTEX point
       } else if (indicator === 3 && !this.defaultBehavior) {
+        // create stability criterion hull to this.sc
         this.THull.stabilityCriterion(auid);
         this.group.add(this.THull.sc);
+
+        // create distance to sc hull
         this.distanceToHull(pt, index, this.THull.sc);
         this.group.add(this.lineGroup);
+
+        // remove original hull
         this.THull.hullGroup.remove(this.THull.hullMesh);
         this.THull.hullGroup.remove(this.THull.edges);
       }
-    } else if (!this.defaultBehavior) {
+    } else if (!this.defaultBehavior) { // if there is no intersection and not default behavior
+      // non default condition because this section removes decomposition points/sc/lines/distance to hull line
+      // on default, these lines remain
+
+      // remove decomposition points/distance to hull line
       this.group.remove(this.lineGroup);
-      this.group.remove(this.THull.sc);
-      this.THull.sc = undefined;
       this.lineGroup.remove(...this.lineGroup.children);
+
+      // the following only need to occur when it is a vertex point
+      // remove stability criterion
+      this.group.remove(this.THull.sc);
+      this.THull.sc = undefined; // set to undefined to indicate to THull that sc will recalculate at next vertex selection
+
+      // re-adds original hull edges and hull
       this.THull.hullGroup.add(this.THull.edges);
       this.THull.hullGroup.add(this.THull.hullMesh);
     }
+    // re-render
     this.render();
   }
 
@@ -561,29 +656,56 @@ class TernaryHullRender {
     // console.log('mouse', this.mouse);
     // calculate objects intersecting the picking ray
     const intersects = this.raycaster.intersectObjects(this.intersectArray);
+
+
     if (intersects.length > 0) {
+      // retrieve auid
       const auid = intersects[0].object.pointNames[intersects[0].index];
       const intersection = (intersects.length) > 0 ? intersects[0] : null;
       // console.log('selecting point: ', auid );
+
+      // point handler (reducer function to manipulate data)
       this.pointClickHandler(auid);
+
+      // retrieve indicator number
       const indicator = this.pointIndicator(auid);
+
+      // retrieve the index from pointNames array
       const index = this.pointCloud.pointNames.indexOf(auid);
+
+      // retrieve the position from position array
       const pt = this.pointCloud.geometry.attributes.position.array.slice(index * 3, index * 3 + 3);
+
+      // CLICKED, NONBINARY/BINARY, NONVERTEX point
       if (this.pointIndicator(auid) === 1) {
+        // add to lineGroup the distance to hull/decomposition points
         this.distanceToHull(pt, auid, this.THull.hullMesh);
         this.group.add(this.lineGroup);
       } else if (indicator === 3) {
+        // create stability criterion hull to this.sc
         this.THull.stabilityCriterion(auid);
         this.group.add(this.THull.sc);
+
+        // create distance to sc hull
         this.distanceToHull(pt, index, this.THull.sc.children[0]);
         this.group.add(this.lineGroup);
+
+        // remove original hull
         this.THull.hullGroup.remove(this.THull.hullMesh);
         this.THull.hullGroup.remove(this.THull.edges);
+      // nothing clicked
       } else {
+        // remove distance to hull/decomposition points
         this.group.remove(this.lineGroup);
+
+        // remove sc hull
         this.group.remove(this.THull.sc);
         this.THull.sc = undefined;
+
+        // clean lineGroup
         this.lineGroup.remove(...this.lineGroup.children);
+
+        // re-add original hull
         this.THull.hullGroup.add(this.THull.edges);
         this.THull.hullGroup.add(this.THull.hullMesh);
       }
@@ -593,6 +715,7 @@ class TernaryHullRender {
     }
   }
 
+  // custom context menu (in development)
   onContextMenu(event) {
     event.preventDefault();
     const viewport = document.getElementById('viewport'); // boostrap fix
